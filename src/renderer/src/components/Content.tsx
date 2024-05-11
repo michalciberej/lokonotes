af8756764b9d0ac513a91abe0a80ea9571db733c
Michal Ciberej <michal.ciberej@gmail.com>
2024-05-11 21:50:41 +0200

    Refactor

--- src/renderer/src/components/Content.tsx
+++ src/renderer/src/components/Content.tsx
@@ -1,3 +1,5 @@
-export const Content = (): JSX.Element => {
-  return <div className="border-l border-neutral-700/70 flex-1 p-2">Content</div>
+export const Content = ({ children }: { children: React.ReactNode }): JSX.Element => {
+  return <div className="border-l border-neutral-700/70 flex-1 overflow-auto">{children}</div>
 }
+
+Content.displayName = 'Content'
