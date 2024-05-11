e3637e32b714d120c16b9abfd4108a242f85009f
Michal Ciberej <michal.ciberej@gmail.com>
2024-05-11 21:57:44 +0200

    Add useEffect to load Notes into store

--- src/renderer/src/components/RootLayout.tsx
+++ src/renderer/src/components/RootLayout.tsx
@@ -1,3 +1,13 @@
+import { useAppDispatch } from '@renderer/hooks/useStore'
+import { getNotes } from '@renderer/store/notesSlice'
+import { useEffect } from 'react'
+
 export const RootLayout = ({ children }: { children: React.ReactNode }): JSX.Element => {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(getNotes())
+  }, [dispatch])
+
   return <main className="flex bg-neutral-900/80 backdrop-blur-3xl h-full">{children}</main>
 }
