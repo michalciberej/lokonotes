cdd69a9b72e876bd0efb8927bd409117932e8a71
Michal Ciberej <michal.ciberej@gmail.com>
2024-05-11 21:45:53 +0200

    Add ActionButtonList

--- src/renderer/src/components/Sidebar.tsx
+++ src/renderer/src/components/Sidebar.tsx
@@ -1,21 +1,9 @@
-import { Button } from './Button'
-import { FaRegTrashAlt, FaPlus } from 'react-icons/fa'
+import { ActionButtonList } from '@components'
 
 export const Sidebar = ({ children }: { children: React.ReactNode }): JSX.Element => {
   return (
-    <aside className="hover:w-1/5 w-24 transition-all p-2 bg-neutral-800/90 duration-300 flex flex-col gap-y-4">
-      <ul className="flex justify-between gap-x-2">
-        <li>
-          <Button aria-label="Create note" className="text-lg">
-            <FaPlus />
-          </Button>
-        </li>
-        <li>
-          <Button aria-label="Delete note" className="text-lg">
-            <FaRegTrashAlt />
-          </Button>
-        </li>
-      </ul>
+    <aside className="hover:w-1/5 w-24 transition-all p-2 bg-neutral-800/90 duration-300 flex flex-col gap-y-2">
+      <ActionButtonList />
       {children}
     </aside>
   )
