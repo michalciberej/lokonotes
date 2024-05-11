50375958fa9f2e1f5d14485307573ce9271bdaf5
Michal Ciberej <michal.ciberej@gmail.com>
2024-05-11 21:43:18 +0200

    Add store provider

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,13 +1,19 @@
-import { Content, RootLayout, Sidebar, NoteList } from './components'
+import { Content, RootLayout, Sidebar, NoteList, Editor } from '@components'
+import { Provider } from 'react-redux'
+import store from './store'
 
 const App = (): JSX.Element => {
   return (
-    <RootLayout>
-      <Sidebar>
-        <NoteList />
-      </Sidebar>
-      <Content />
-    </RootLayout>
+    <Provider store={store}>
+      <RootLayout>
+        <Sidebar>
+          <NoteList />
+        </Sidebar>
+        <Content>
+          <Editor />
+        </Content>
+      </RootLayout>
+    </Provider>
   )
 }
 
