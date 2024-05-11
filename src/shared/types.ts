bf0ec37d39c5cf9b55e64684df8958b0a53b4020
Michal Ciberej <michal.ciberej@gmail.com>
2024-05-11 21:29:57 +0200

    Refactor NoteType declaration

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,14 +1,12 @@
 export interface NoteType {
   title: string
-  lastUpdate: number
-}
-
-export interface NoteWithContent extends NoteType {
+  lastUpdate: Date
   content: string
 }
+export type NoteContentType = string
 
 export type CreateNote = () => Promise<NoteType['title'] | null>
-export type DeleteNote = (fileName: string) => Promise<true | null>
-export type ReadNote = (fileName: string) => Promise<string>
-export type WriteNote = (fileName: string, dataToWrite: string) => Promise<void>
+export type DeleteNote = (fileName: NoteType['title']) => Promise<true | null>
+export type ReadNote = (fileName: NoteType['title']) => Promise<NoteContentType>
+export type WriteNote = (fileName: NoteType['title'], dataToWrite: string) => Promise<void>
 export type GetNotes = () => Promise<NoteType[] | null>
