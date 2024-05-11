8687e304cab63348df4771fa43ca5ae5e9ed2a4f
Michal Ciberej <michal.ciberej@gmail.com>
2024-05-11 21:54:51 +0200

    Change props type, and refactor, remove return type

--- src/renderer/src/components/Note.tsx
+++ src/renderer/src/components/Note.tsx
@@ -1,8 +1,20 @@
-export const Note = ({ note }: { note: { title: string; lastUpdate: string } }): JSX.Element => {
+import type { NoteType } from '@shared/types'
+import { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+type NoteProps = Omit<NoteType, 'content'> & ComponentProps<'div'>
+
+export const Note = ({ title, lastUpdate, className, ...props }: NoteProps) => {
   return (
-    <div className="flex flex-col gap-y-2 border border-neutral-700/50 p-2 rounded-md">
-      <p>{note.title}</p>
-      <p className="line-clamp-1">{note.lastUpdate}</p>
+    <div
+      className={twMerge(
+        'flex flex-col gap-y-2 border hover:bg-neutral-500/30 border-neutral-700/50 p-2 rounded-md transition-colors',
+        className
+      )}
+      {...props}
+    >
+      <p className="line-clamp-1">{title}</p>
+      <p className="line-clamp-1 text-sm">{lastUpdate.toString()}</p>
     </div>
   )
 }
