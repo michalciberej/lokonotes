cacf305d9f6c0a45d061916ca6db0dca707277f0
Michal Ciberej <michal.ciberej@gmail.com>
2024-05-11 22:01:41 +0200

    Link to store, add handleNote and  removeNote

--- src/renderer/src/components/ActionButtonList.tsx
+++ src/renderer/src/components/ActionButtonList.tsx
@@ -1,18 +1,37 @@
 import { FaPlus, FaRegTrashAlt } from 'react-icons/fa'
 import { Button } from '@components'
-import { useStore } from '@renderer/store'
+import { useAppDispatch, useAppSelector } from '@renderer/hooks/useStore'
+import { addNote, deleteNote } from '@renderer/store/notesSlice'
+import { RootState } from '@renderer/store'
 
 export const ActionButtonList = (): JSX.Element => {
-  const addNote = useStore((state) => state.addNote)
+  const dispatch = useAppDispatch()
+  const notes = useAppSelector((state: RootState) => state.notes.notes)
+  const currentlyActive = useAppSelector((state: RootState) => state.notes.currentlyActive)
+
+  const handleAddNote = async () => {
+    const noteTitle = await window.context.createNote()
+
+    if (!noteTitle) return
+
+    dispatch(addNote(noteTitle))
+  }
+
+  const handleDeleteNote = async () => {
+    const noteTitle = notes[currentlyActive].title
+    const succesfulyDeletedFile = await window.context.deleteNote(noteTitle)
+    if (succesfulyDeletedFile) dispatch(deleteNote(noteTitle))
+  }
+
   return (
     <ul className="flex justify-between gap-x-2">
       <li>
-        <Button aria-label="Create note" className="text-lg" onClick={addNote}>
+        <Button aria-label="Create note" className="text-lg" onClick={handleAddNote}>
           <FaPlus />
         </Button>
       </li>
       <li>
-        <Button aria-label="Delete note" className="text-lg">
+        <Button aria-label="Delete note" className="text-lg" onClick={handleDeleteNote}>
           <FaRegTrashAlt />
         </Button>
       </li>
