3a80242558f4117ff5dfbcb1155cc5422b959d65
Michal Ciberej <michal.ciberej@gmail.com>
2024-05-11 21:52:02 +0200

    Remove return type

--- src/renderer/src/components/Content.tsx
+++ src/renderer/src/components/Content.tsx
@@ -1,4 +1,4 @@
-export const Content = ({ children }: { children: React.ReactNode }): JSX.Element => {
+export const Content = ({ children }: { children: React.ReactNode }) => {
   return <div className="border-l border-neutral-700/70 flex-1 overflow-auto">{children}</div>
 }
 
